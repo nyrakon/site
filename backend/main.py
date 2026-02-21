1543d1dd4dddd30f60c35cada75ff8624619b983
upsilon <obfuu@proton.me>
2026-02-21 11:47:06 -0600

    setup api

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
 from dotenv import dotenv_values
+import os, subprocess
 settings=dotenv_values("../.env") # < env path is outside html
-app=FastAPI(title='NyrakonAPI')
+version=subprocess.check_output("git describe --tags --abbrev=0".split(" ")).decode().strip()
+app=FastAPI(title='NyrakonAPI',version=version)
+
+@app.get("/")
+def root():
+    return {"STATUS":"OK"}
